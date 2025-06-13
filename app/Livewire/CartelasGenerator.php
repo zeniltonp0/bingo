4decef635dd1f96906bc3d6bad320a1aac93d790
Zenilton Pereira <zeniltonp0@gmail.com>
2025-06-13 16:28:13 -0300

    adicionado componente livewire cartelas-generator

---
+++ app/Livewire/CartelasGenerator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CartelasGenerator extends Component
+{
+
+    public function render()
+    {
+        return view('livewire.cartelas-generator');
+    }
+
+    
+}
